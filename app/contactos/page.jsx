d0d651f2f598a3740a22a8dc097a363bc0f8ab68
Nerovelic <zack.vadhir2@hotmail.com>
2023-04-02 18:32:27 -0700

    ahora el contactos es recursivo

--- app/contactos/page.jsx
+++ app/contactos/page.jsx
@@ -1,8 +1,19 @@
-import React from "react";
+'use client'
+import { useEffect, useState } from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMapMarkerAlt, faPhone, faEnvelope  } from '@fortawesome/free-solid-svg-icons';
 
 const Contacts = () => {
+  const [width, setWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWidth(window.innerWidth);
+    };
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+  
   return (
     <div className="bg-gray-100 py-12">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
@@ -41,13 +52,15 @@ const Contacts = () => {
               </dd>
             </div>
             <div className="mt-8 sm:mt-0 flex items-center">
+              <div className="flex">
                 <FontAwesomeIcon icon={faEnvelope} className="mr-3 h-10 w-10" />
-              <dt className="text-lg leading-6 font-medium text-gray-900">
-                Primer Correo 
-              </dt>
-              <dd className="ml-4 text-base text-gray-500">
-                ferreterialainternacionalsa@gmail.com
-              </dd>
+                <dt className="text-lg leading-6 font-medium text-gray-900">
+                  Primer Correo
+                </dt>
+                <dd className="ml-4 text-base text-gray-500">               	
+                  ferreterialainternacionalsa@gmail.com
+                </dd>
+              </div>
             </div>
             <div className="mt-8 sm:mt-0 flex items-center">
                 <FontAwesomeIcon icon={faPhone} className="mr-3 h-10 w-10" />
@@ -59,18 +72,28 @@ const Contacts = () => {
               </dd>
             </div>
             <div className="mt-8 sm:mt-0 flex items-center">
+              <div className="flex">
                 <FontAwesomeIcon icon={faEnvelope} className="mr-3 h-10 w-10" />
-              <dt className="text-lg leading-6 font-medium text-gray-900">
-                Segundo Correo
-              </dt>
-              <dd className="ml-4 text-base text-gray-500">               	
-                ferreteria_lainternacional@hotmail.com
-              </dd>
+                <dt className="text-lg leading-6 font-medium text-gray-900">
+                  Segundo Correo
+                </dt>
+                <dd className="ml-4 text-base text-gray-500 sm:mt-4">
+                  ferreteria_lainternacional@hotmail.com
+                </dd>
+              </div>
             </div>
           </dl>
           <div className="mt-10">
-          <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d13554.474772151412!2d-116.6068819!3d31.8625898!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80d88d7b14539aa1%3A0x5c1d5156b7a4869!2sFerreter%C3%ADa%20y%20Maderer%C3%ADa%20la%20Internacional!5e0!3m2!1ses-419!2smx!4v1680478363577!5m2!1ses-419!2smx" width="1216" height="450" style={{ border: 0 }} allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
-          </div>
+              <iframe
+                src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d13554.474772151412!2d-116.6068819!3d31.8625898!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80d88d7b14539aa1%3A0x5c1d5156b7a4869!2sFerreter%C3%ADa%20y%20Maderer%C3%ADa%20la%20Internacional!5e0!3m2!1ses-419!2smx!4v1680478363577!5m2!1ses-419!2smx"
+                width={width > 768 ? 1216 : 400}
+                height={width > 768 ? 450 : 300}
+                style={{ border: 0 }}
+                allowFullScreen=""
+                loading="lazy"
+                referrerPolicy="no-referrer-when-downgrade"
+              ></iframe>
+           </div>
         </div>
       </div>
     </div>
