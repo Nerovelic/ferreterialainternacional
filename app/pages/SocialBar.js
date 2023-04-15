cd78e5544e1cc1d300f556ae084db87f13bd39ba
Nerovelic <zack.vadhir2@hotmail.com>
2023-04-14 20:07:59 -0700

    se añadio la red social de instegram en la SocialBar

--- app/pages/SocialBar.js
+++ app/pages/SocialBar.js
@@ -6,6 +6,9 @@ const SocialBar = () => {
       <a href="https://www.facebook.com/lainternacional.materiales" target="_blank" rel="">
         <i className="fab fa-facebook-f"></i>
       </a>
+      <a href="https://www.instagram.com/lainternacionalferreteriay/?fbclid=IwAR08ow30fqOwsgz4kluqZPbhoylhNvhGrXqrG7MYX6JatZjwrD1dPvL9Cko" target="_blank" rel="">
+        <i className="fab fa-instagram"></i>
+      </a>
     </div>
   );
 };
